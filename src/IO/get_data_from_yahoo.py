cfeac1d21f107f670d182d10c887fac3b5d46aad
lmcango <andre.godin@ericsosn.com>
2020-11-12 14:19:58 -0500

    assignment final

--- src/IO/get_data_from_yahoo.py
+++ src/IO/get_data_from_yahoo.py
@@ -4,10 +4,13 @@ from yahoo_fin import stock_info as si
 
 
 def get_last_stock_price(ticker, last=False):
-    tickers = si.tickers_sp500()
-    print(tickers)
+    #tickers = si.tickers_sp500()
     if last:
         now = datetime.now()
         start_date = now - timedelta(days=60)
         return si.get_data(ticker, start_date)
     return si.get_data(ticker)
+
+def get_all_tickers():
+    tickers = si.tickers_sp500()
+    return tickers
