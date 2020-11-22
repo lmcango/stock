a819804cf7680da2d9b9860297a1e299bd6dc3d4
lmcango <andre.godin@ericsosn.com>
2020-11-22 14:11:11 -0500

    assignment final3

--- src/IO/get_data_from_yahoo.py
+++ src/IO/get_data_from_yahoo.py
@@ -2,9 +2,17 @@ from datetime import datetime, timedelta
 
 from yahoo_fin import stock_info as si
 
+import pandas as pd
+
 
 def get_last_stock_price(ticker, last=False):
     #tickers = si.tickers_sp500()
+    try:
+        si.get_data(ticker)
+
+    except Exception as e:
+        return pd.DataFrame()
+
     if last:
         now = datetime.now()
         start_date = now - timedelta(days=60)
